6b796a0d2922f16afe0a20d2e4e592cc0082d3f1
Collin Mullis <cmullis@saintjoe.edu>
2015-02-18 13:08:14 -0500

    program will use 2 functions, Square, and Cube, to square a number, then print the result, then cube a different or same number, and print that result.

---
+++ test3.js
@@ -0,0 +1,13 @@
+/* Code by Collin Mullis
+	CMP237
+	Practicum Test 3 Functions 
+*/
+function square(x) {
+	return x * x
+	}
+console.log(square(2))
+
+var cube = function(y) {
+	return y * y * y
+}
+console.log(cube(2))
