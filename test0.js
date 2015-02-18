2ce9ca9e3ef881b5f4fc0ce16df5ef4ad509298f
ColdC <cmullis@saintjoe.edu>
2015-02-18 13:32:32 -0500

    Added comments to save my life.

--- test0.js
+++ test0.js
@@ -5,11 +5,13 @@
 
 // i variable used for counter
 for (var i = 0; i <= 100; i++) {
-	
+// checks to see if "i" is even, then prints "I'm an even" if true.	
 	if (i % 2 == 0)
 		console.log("I'm an even" + " " + i);
+// checks to see if "i" is odd, then prints "I'm an odd one" if true.		
 	else if (i % 2 != 0) 
 		console.log("I'm an odd one" + " " + i);
 	}
+// prints "Yay, you can count to 100 now!" After the for loop has finished.
 	console.log("Yay, you can count to 100 now!")
 	
