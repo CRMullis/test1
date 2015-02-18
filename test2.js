73edbbcc2996dee7f234c816e4c7632c8aeae7f2
ColdC <cmullis@saintjoe.edu>
2015-02-18 13:36:47 -0500

    Added comments to save my life
    Corrected comments also.

--- test2.js
+++ test2.js
@@ -1,13 +1,16 @@
 /* Code by Collin Mullis
 	CMP237
-	Practicum Test 2 Functions 
+	test 2 practicum
 */
+// creates function called "square" which takes a single argument of "x" and then squares the number.
 function square(x) {
 	return x * x
 	}
+// Prints result of function with argument "2"
 console.log(square(2))
-
+// creates function called "cube" which takes a single argument of "y" and then cubes the number.
 var cube = function(y) {
 	return y * y * y
 }
+// Prints the result of function with argument "2"
 console.log(cube(2))
