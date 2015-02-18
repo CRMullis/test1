60115a6e388f90b1d5f050a29233c33fa05e9843
Collin Mullis <cmullis@saintjoe.edu>
2015-02-18 12:35:30 -0500

    Program prints out 1-100, telling you if it is even or odd, then will print "Yay, you can count to 100" to demonstrate 3 Control Flows

--- test0.js
+++ test0.js
@@ -1,2 +1,15 @@
-for (var i = 0; i <= 100; i++)
-	console.log(i)
+/* Code by Collin Mullis
+	CMP237 Exam 1 Practicum
+	3 Control Flows
+*/
+
+// i variable used for counter
+for (var i = 0; i <= 100; i++) {
+	
+	if (i % 2 == 0)
+		console.log("I'm an even" + " " + i);
+	else if (i % 2 != 0) 
+		console.log("I'm an odd one" + " " + i);
+	}
+	console.log("Yay, you can count to 100 now!")
+	
