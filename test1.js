9b5263f241b72080f2017c4eb2a517aa3defbccb
ColdC <cmullis@saintjoe.edu>
2015-02-18 13:34:58 -0500

    Added comments to save my life
    Also corrected some comments

--- test1.js
+++ test1.js
@@ -1,9 +1,14 @@
 /* Code by Collin Mullis
 	CMP237
-	Practicum #1
+	test 1 practicum
 */
+// creates variable named "Num"
 var Num = 1;
+// prints "Num"
 	console.log (Num);
+// Checks to see if "Num" is itself
 if (Num == Num)
+// multiplys "Num" by 2 to change variable
 	Num = Num * 2;
+// prints result of math
 console.log (Num);
